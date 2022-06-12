d5df51b58171359bd980aee42feafe95cb8544eb
voooc <xiaojuan2021@gmail.com>
2022-06-12 17:55:27 +0800

    更改结构，配置登录页面和主页面的路由

--- src/App.vue
+++ src/App.vue
@@ -1,93 +1,32 @@
 <template>
   <div class="container">
     <global-header :user="currentUser"></global-header>
-    <validate-form @form-submit="onFormSubmit">
-      <div class="mb-3">
-        <label class="form-label">邮箱地址</label>
-        <validate-input :rules="emailRules" v-model="emailVal" placeholder="请输入邮箱地址" type="text"></validate-input>
-      </div>
-      <div class="mb-3">
-        <label class="form-label">密码</label>
-        <validate-input :rules="passwordRules" v-model="passwordVal" placeholder="请输入密码" type="password"></validate-input>
-      </div>
-      <template v-slot:submit>
-        <span class="btn btn-danger">Submit</span>
-      </template>
-    </validate-form>
-    <column-list :list="list"></column-list>
+    <router-view></router-view>
+    <global-footer></global-footer>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import ColumnList, { ColumnProps } from './components/ColumnList.vue'
 import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
-import ValidateInput, { RuleProps } from './components/ValidateInput.vue'
-import ValidateForm from './components/ValidateForm.vue'
+import GlobalFooter from './components/GlobalFooter.vue'
 const currentUser: UserProps = {
-  isLogin: true,
-  name: 'vikng'
+  isLogin: false,
+  name: ''
 }
-const testData: ColumnProps[] = [
-  {
-    id: 1,
-    title: 'test1的专栏',
-    description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 2,
-    title: 'test2的专栏',
-    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 3,
-    title: 'test3的专栏',
-    description: '这是的test3专栏，有一段非常有意思的简介，可以更新一下欧'
-    // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 4,
-    title: 'test4的专栏',
-    description: '这是的test4专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  }
-]
+
 export default defineComponent({
   name: 'App',
   components: {
-    ColumnList,
     GlobalHeader,
-    ValidateInput,
-    ValidateForm
+    GlobalFooter
+    // Home,
+    // Login
   },
   setup () {
-    // email
-    const emailVal = ref('')
-    const emailRules: RuleProps = [
-      { type: 'required', message: '电子邮箱地址不能为空' },
-      { type: 'email', message: '请输入正确的电子邮箱格式' }
-    ]
-    // password
-    const passwordVal = ref('')
-    const passwordRules: RuleProps = [
-      { type: 'required', message: '密码不能为空' },
-      { type: 'password', message: '请输入数字和字母组合的8-16位密码' }
-    ]
-    // 输出表单处理结果
-    const onFormSubmit = (result: boolean) => {
-      console.log(result)
-    }
     return {
-      list: testData,
-      currentUser,
-      emailRules,
-      emailVal,
-      passwordVal,
-      passwordRules,
-      onFormSubmit
+      currentUser
     }
   }
 })
---
+++ src/views/Login.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <validate-form @form-submit="onFormSubmit">
+      <div class="mb-3">
+        <label class="form-label">邮箱地址</label>
+        <validate-input :rules="emailRules" v-model="emailVal" placeholder="请输入邮箱地址" type="text"></validate-input>
+      </div>
+      <div class="mb-3">
+        <label class="form-label">密码</label>
+        <validate-input :rules="passwordRules" v-model="passwordVal" placeholder="请输入密码" type="password"></validate-input>
+      </div>
+      <template v-slot:submit>
+        <button type="submit" class="btn btn-primary btn-block btn-large">登录</button>
+      </template>
+    </validate-form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ValidateInput, { RuleProps } from '../components/ValidateInput.vue'
+import ValidateForm from '../components/ValidateForm.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    ValidateInput,
+    ValidateForm
+  },
+  setup () {
+    // email
+    const emailVal = ref('')
+    const emailRules: RuleProps = [
+      { type: 'required', message: '电子邮箱地址不能为空' },
+      { type: 'email', message: '请输入正确的电子邮箱格式' }
+    ]
+    // password
+    const passwordVal = ref('')
+    const passwordRules: RuleProps = [
+      { type: 'required', message: '密码不能为空' },
+      { type: 'password', message: '请输入数字和字母组合的8-16位密码' }
+    ]
+    // 输出表单处理结果
+    const onFormSubmit = (result: boolean) => {
+      console.log(result)
+    }
+    return {
+      emailRules,
+      emailVal,
+      passwordVal,
+      passwordRules,
+      onFormSubmit
+    }
+  }
+})
+</script>
